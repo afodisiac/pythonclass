34e73c6d814910a0b2dd83b72b935606eaf6cdaf
Nyoko674727 <67692880+nyoko674727@users.noreply.github.com>
2024-02-07 01:42:04 -0800

    functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,50 @@
-def function1():
-    return("Hello")
+def calculate_total(num1, num2):
+    total = num1 + num2 
+    print("Total:", total)
+ 
+def client():
+    num1 = float(input("enter number"))
+    num2 = float(input("enter number"))
+    client_total = (num1 + num2)
+    print("Total:", client_total)
 
-def func2():
-    x=function1()
-    print(x,  "world")
+client()
 
-func2()
+def calculate_differnce(num1, num2):
+    difference = num1 - num2 
+    print("Difference:", difference)
+ 
+def client():
+    num1 = float(input("enter number"))
+    num2 = float(input("enter number"))
+    client_differnce = (num1 - num2)
+    print("Total:", client_differnce)
+
+client()
+
+def calculate_dvision(num1, num2):
+    division = num1 / num2 
+    print("Difference:", division)
+ 
+def client():
+    num1 = float(input("enter number"))
+    num2 = float(input("enter number"))
+    client_division = (num1 / num2)
+    print("Total:", client_division)
+
+client()
+
+def calculate_mulitiplication(num1, num2):
+    multiplication = num1 * num2 
+    print("Difference:", multiplication)
+ 
+def client():
+    num1 = float(input("enter number"))
+    num2 = float(input("enter number"))
+    client_multiplication = (num1 * num2)
+    print("answer:", client_multiplication)
+
+client()
+
+   
+  
